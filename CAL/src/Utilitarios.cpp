60b512b96db58bd61eeaae6bb764d01211c42907
José Alexandre Teixeira <jabst1@hotmail.com>
2015-03-31 10:36:04 +0100

    1 commit

---
+++ CAL/src/Utilitarios.cpp
@@ -0,0 +1,26 @@
+#include "Utilitarios.h"
+
+
+double distancia(Hotspot & obj1, Hotspot & obj2) {
+
+	return sqrt(pow(obj1.getCoordY() - obj2.getCoordY(), 2.0) + pow(obj1.getCoordX() - obj2.getCoordY(), 2.0));
+}
+
+double calc_dist(vector<Nodes<Hotspot> *> const &g1) {
+	double dist = DMAX, temp = 0;
+
+
+	for (unsigned int i = 0; i < g1.size(); i++){
+		for (unsigned int j = 0; j < g1.size(); j++){
+			if (i == j)
+				continue;
+			temp = distancia(g1.at(i)->getInfo(), g1.at(j)->getInfo());
+			if (dist > temp){
+				dist = temp;
+			}
+		}
+	}
+
+	return dist;
+
+}
