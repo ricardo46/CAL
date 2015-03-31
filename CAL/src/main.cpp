9e5026f24e35772e527bbf5cbf15dbaeee1a8005
José Alexandre Teixeira <jabst1@hotmail.com>
2015-03-31 10:37:22 +0100

    main

--- CAL/src/main.cpp
+++ CAL/src/main.cpp
@@ -1,14 +1,17 @@
-
+#include "Utilitarios.h"
 #include "Graph.h"
 #include "Hotspot.h"
 #include <string>
 
 using namespace std;
 
+
 int main(){
 
 	Graph<Hotspot> g1;
 	ifstream file;
+
+	//Fazer o LOAD
 	file.open("nodes.txt");
 	string n, x, y;
 	while (!file.eof()){
@@ -21,11 +24,14 @@ int main(){
 	}
 
 	file.close();
+	//
 
 	for (unsigned int i = 0; i < g1.getVertexSet().size(); i++){
-		cout << g1.getVertexSet()[i]->getInfo().getNome() << endl;
+		cout << g1.getVertexSet()[i]->getInfo() << endl;
 	}
-
+	
+	cout << calc_dist(g1.getVertexSet());
+	
 	
 	system("pause");
 	return 0;
